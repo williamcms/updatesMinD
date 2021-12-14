aecb466c655988453b50c53c0e51718cd667c99d
williamcms <williamcms_@outlook.com>
2021-12-14 14:19:08 -0300

    initial commit

---
+++ _REFORM/arquivos/MIND-reform-casamind.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('.slick-carousel-dots').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: false,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+        nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+	$('.slick-carousel-arrows').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: false,
+		arrows: true,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+        nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+	$('.slick-carousel-both').slick({
+		autoplay: true,
+		autoplaySpeed: 3000,
+		dots: true,
+		arrows: true,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+        nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+});
