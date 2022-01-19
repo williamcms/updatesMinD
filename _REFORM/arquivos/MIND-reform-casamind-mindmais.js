6ec7f53a8ad04d25eae1cb91688e10ed9945e1ac
williamcms <williamcms_@outlook.com>
2022-01-19 14:36:27 -0300

    Inicio do desenvolvimento do MinD+

--- _REFORM/arquivos/MIND-reform-casamind-mindmais.js
+++ _REFORM/arquivos/MIND-reform-casamind-mindmais.js
@@ -2,3 +2,21 @@
  *	Código por William Di Biasi Bogik
  *
  */
+ $(document).ready(function(){
+    "use strict";
+    var getSellersList = (function = () =>{
+        var sellers = {};
+
+        $.ajax({
+            accept: "application/vnd.vtex.ds.v10+json",
+            contentType: "application/json; charset=utf-8",
+            crossDomain: false,
+            type: "GET",
+            url: "/arquivos/mindmais.json"
+        }).done(function(e){
+            sellers = $.map(e, function(r){return r.sellers});
+            sellers.sort((a,b) => a.name.localeCompare(b.name));
+        });
+        return sellers;
+    });
+}
