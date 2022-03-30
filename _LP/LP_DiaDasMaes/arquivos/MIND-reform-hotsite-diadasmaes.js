c9df7496cb23ad089acadbff19341ffebab32c79
williamcms <williamcms_@outlook.com>
2022-03-30 17:29:28 -0300

    Ajustes: melhorias de layout da LP

--- _LP/LP_DiaDasMaes/arquivos/MIND-reform-hotsite-diadasmaes.js
+++ _LP/LP_DiaDasMaes/arquivos/MIND-reform-hotsite-diadasmaes.js
@@ -1,16 +1,62 @@
-$('main.hotsite .slick-collections').slick({
-	autoplay: false,
-	autoplaySpeed: 3000,
-	dots: false,
-	arrows: true,
-	prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
-	nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
-	lazyLoad: 'ondemand',
-	accessibility: true,
-	infinite: true,
-	swipeToSlide: true,
-	centerMode: true,
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	swipeToSlide: true
-});
+$(document).ready(function(){
+	$('main.hotsite .slick-collections').slick({
+		autoplay: false,
+		autoplaySpeed: 3000,
+		dots: false,
+		arrows: true,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+		nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		accessibility: true,
+		infinite: true,
+		swipeToSlide: true,
+		centerMode: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 832,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 500,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
+	$('main.hotsite .slick-destaq').slick({
+		autoplay: false,
+		autoplaySpeed: 3000,
+		dots: false,
+		arrows: true,
+		prevArrow: '<button class="slick-prev" aria-label="Anterior" type="button">Anterior</button>',
+		nextArrow: '<button class="slick-next" aria-label="Próximo" type="button">Próximo</button>',
+		lazyLoad: 'ondemand',
+		accessibility: true,
+		infinite: true,
+		swipeToSlide: true,
+		centerMode: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 832,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+		},
+		{
+			breakpoint: 500,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
+})
