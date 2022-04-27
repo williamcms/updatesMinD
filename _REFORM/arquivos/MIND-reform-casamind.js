e6b51946b5ca253a06d7d5ad6be7551ebe1fe750
williamcms <williamcms_@outlook.com>
2022-04-27 16:38:02 -0300

    Pequenas alterações estéticas no código

--- _REFORM/arquivos/MIND-reform-casamind.js
+++ _REFORM/arquivos/MIND-reform-casamind.js
@@ -6,7 +6,7 @@
 	let isMobile = false; //initiate as false
 	//device detection
 	if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
-		|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) { 
+		|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))){ 
 		isMobile = true;
 	}
 	return isMobile;
@@ -23,7 +23,7 @@ $(document).ready(function(){
 	//Addons
 	if(typeof(Array.prototype.first) === 'undefined'){
 		Object.defineProperty(Array.prototype, 'first', {
-			value() {
+			value(){
 				return this.find(e => true)     //or this.find(Boolean)
 			}
 		});
@@ -52,7 +52,7 @@ $(document).ready(function(){
 		});
 	}
 	//Atualiza os parametros do url
-	function updateQueryString(key, value) {
+	function updateQueryString(key, value){
 		if(history.pushState){
 			let searchParams = new URLSearchParams(window.location.search);
 			searchParams.set(key, value);
@@ -100,7 +100,7 @@ $(document).ready(function(){
 			scrollDirection: 'both',
 			effect: 'fadeIn',
 			visibleOnly: true,
-			onError: function(e) {
+			onError: function(e){
 				console.log('error loading ' + e.data('src'));
 			},
 			afterLoad: function(e){
@@ -177,9 +177,9 @@ $(document).ready(function(){
 			$(this).parents('.csm-dropdown').removeClass('change');
 		}
 	});
-	var hideMenuOnScroll = (hideMenuOnScroll = () => {
+	var hideMenuOnScroll = (hideMenuOnScroll = () =>{
 		let top = 0;
-		$(window).on("scroll", function() {
+		$(window).on("scroll", function(){
 			$(window).scrollTop() > top && 1 < $(window).scrollTop() ? ($(".csm-header .csm-wrapper").slideUp(), 
 			$(".csm-header .csm-wrapper .csm-middle .csm-center").addClass("scrollMenu"), $(".csm-header .csm-minicart").addClass("scrollMenu"), $(".csm-header .csm-benefits").slideUp(), $(".csm-header #popup-adicionando").removeClass("scrollMenu").addClass("hiddenMenu")) : $(window).scrollTop() === 0 ? ($(".csm-header .csm-benefits").slideDown(), 
 			$(".csm-header .csm-wrapper").slideDown(), $(".csm-header .csm-wrapper .csm-middle .csm-center").removeClass("scrollMenu"), $(".csm-header .csm-minicart").removeClass("scrollMenu"), $(".csm-header #popup-adicionando").removeClass("scrollMenu hiddenMenu")) : $(".csm-header .csm-wrapper").slideDown(), 
@@ -192,11 +192,11 @@ $(document).ready(function(){
 		let t = $(this),
 		e = $(".csm-minicart"), 
 		i = $(".portal-totalizers-ref .amount-items-em").eq(0).text();
-		(768 <= $("body").width()) ? (t.hover(function(t) {
+		(768 <= $("body").width()) ? (t.hover(function(t){
 			if(parseInt($(".cart-info > .amount-items > em.amount-items-em").first().text()) != 0) return e.addClass("is--active"), false;
-		}), e.hover(function() {}, function() {
+		}), e.hover(function(){}, function(){
 			e.removeClass("is--active");
-		})) : t.click(function(t) {
+		})) : t.click(function(t){
 			if(parseInt($(".cart-info > .amount-items > em.amount-items-em").first().text()) != 0) return e.toggleClass("is--active"), false;
 		});
 	});
@@ -211,7 +211,7 @@ $(document).ready(function(){
 			};
 			return vtexjs.checkout.updateItems([updateItem], null, false);
 		})
-		.done(function(orderForm) {
+		.done(function(orderForm){
 			//console.log(orderForm);
 			updateMiniCart();
 			(qty == 0 && $('.csm-minicart__products > ul > li')[itemIndex].remove());
@@ -220,7 +220,7 @@ $(document).ready(function(){
 	//Scripts que serão acionados com base no orderForm
 	var checkOrderForm = (checkOrderForm = () =>{
 		let minicart = $('.csm-header .csm-middle .csm-center .csm-user > ul > li.csm-cart > a');
-		vtexjs.checkout.getOrderForm().done(function(t) {
+		vtexjs.checkout.getOrderForm().done(function(t){
 			(t.items.length > 0 && updateMiniCart());
 		});
 	})();
@@ -233,7 +233,7 @@ $(document).ready(function(){
 		minicartIcon.attr('aria-label', 'Você possui ' + item + ' ' + (item == 1 ? 'item' : 'itens') + ' no Carrinho');
 		minicartIcon.attr('title', 'Você possui ' + item + ' ' + (item == 1 ? 'item' : 'itens') + ' no Carrinho');
 
-		vtexjs.checkout.getOrderForm().done(function(t) {
+		vtexjs.checkout.getOrderForm().done(function(t){
 			$.each(t.items, function(i){
 				if(p.find('li[data-id='+ this.id +']').length == 0){
 					p.append('<li data-id="'+ this.id +'" available-qty="'+ getSkuData(this.id).SkuSellersInformation.first().AvailableQuantity
@@ -632,7 +632,7 @@ $(document).ready(function(){
 			let buybutton = $('a.buy-button.buy-button-ref').attr('href');
 			var params = {};
 			buybutton.replace(/[?&]+([^=&]+)=([^&]*)/gi,
-				function (str, key, value) {
+				function(str, key, value){
 					params[key] = value;
 				});
 			return vtexjs.checkout.addToCart([{
@@ -662,7 +662,7 @@ $(document).ready(function(){
 			verticalSwiping: true,
 			centerMode: true
 		});
-		var availableAlert = (function() {
+		var availableAlert = (function(){
 			try{
 				let t = skuJson.productId;
 				$.ajax({
@@ -672,7 +672,7 @@ $(document).ready(function(){
 					headers: {
 						"Content-Type": "application/json"
 					}
-				}).done(function(t) {
+				}).done(function(t){
 					let e = t[0].items[0].sellers[0].commertialOffer.AvailableQuantity, 
 					b = t[0].brand;
 					0 < e && e <= 5 && $(".available-alert").html('<p>Corre que só tem <strong>' + e + "</strong> unidades disponíveis!</p>"), 
@@ -686,7 +686,7 @@ $(document).ready(function(){
 		//É possivel trocar a dimensão no CMS > Configurações > Tipos de Arquivo,
 		//mas afeta todos os sites
 		var changeProductImage = (changeProductImage = () =>{
-			$("ul.thumbs img").each(function() {
+			$("ul.thumbs img").each(function(){
 				$(this).closest('a').attr("rel", 
 					$(this).closest('a').attr("rel").replace("-500-500", "-500-750"));
 				$(this).attr("src", $(this).attr("src").replace("-350-303", "-90-130"));
@@ -732,7 +732,7 @@ $(document).ready(function(){
 
 			$("#csm-realidadeAumentada").show()
 		})();
-		var pdfDowlond = (function() {
+		var pdfDowlond = (function(){
 			let t = $(".value-field.pdf-dowload").text();
 
 			if(t != ""){
@@ -762,13 +762,13 @@ $(document).ready(function(){
 			data: JSON.stringify(i),
 			type: "POST",
 			url: "/api/dataentities/PU/documents",
-			success: function(e) {
+			success: function(e){
 				$("#formFC").html("<p class='highlight'>E-mail cadastrado com sucesso!</p><small>Você receberá um cupom de desconto em seu e-mail.</small>"), 
-				setTimeout(function() {
+				setTimeout(function(){
 					//t("#newsletter-pop").removeClass("slide");
 				}, 5e3), sessionStorage.setItem("mindNewsletter", true);
 			},
-			error: function(t) {}
+			error: function(t){}
 		})
 	});
 	var verifyNewsletterOnSession = (verifyNewsletterOnSession = () =>{
@@ -827,7 +827,7 @@ $(document).ready(function(){
 		});
 	})();
 	//NeoAssist 
-	var neoAssist = (function () {
+	var neoAssist = (function(){
 		$('body').append('<div id="neoAssist"></div>');
 
 		window.NeoAssistTag = {};
