1cdb46a72a958d17f88e075a0313ec55eb9ec42c
williamcms <williamcms_@outlook.com>
2022-05-26 16:46:19 -0300

    Ampliação das estilizações da página de criação da lista e melhorias gerais no fluxo de criação

--- _REFORM/arquivos/MIND-reform-giftlist.js
+++ _REFORM/arquivos/MIND-reform-giftlist.js
@@ -18,7 +18,20 @@ $(document).ready(function(){
 			}
 		})
 	});
-	 $("#giftlisttype").on('change', function(){
+	$('#giftlistform input, #giftlistform select').on('keydown change', function(){
+		let giftlisttype = $('#giftlisttype').find(':selected').text(),
+			giftlistname = $('#giftlistname').val(),
+			creationdate = Date.now();
+
+		let str = giftlisttype + giftlistname + creationdate;
+		// Remove acentuações e espaços
+		let url = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+			url = str.replace(/\s/g, '');
+
+		$('#giftlisturl').val(url);
+		$('#giftlisturl').trigger('change');
+	});
+	$("#giftlisttype").on('change', function(){
 		var _el = $(this);
 		console.log(_el.find(':selected').val())
 		if(_el.find(':selected').val() == 20){
