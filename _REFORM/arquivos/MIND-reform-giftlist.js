958c0a1b50ab0613c7ba6527b5b829e07ce07b7c
williamcms <williamcms_@outlook.com>
2022-05-30 13:42:55 -0300

    Finalização e melhoria da estilização da página de criação da lista, melhoria nos scripts existentes e termino do script de nomeação do link final

--- _REFORM/arquivos/MIND-reform-giftlist.js
+++ _REFORM/arquivos/MIND-reform-giftlist.js
@@ -1,43 +1,82 @@
 $(document).ready(function(){
-	$("body").on("blur", ".address-form-new #ship-postal-code", function(event) {
-		var _element = $(this)
-		, _cep = _element.val();
+	// Função para algumas otimizações, 
+	// como indicador de campo númerico para mobile
+	(() =>{
+		let giftlisteventdate = $('#giftlisteventdate');
+
+		giftlisteventdate.prop('inputmode', 'numeric');
+	})();
+	// Função para preencher alguns campos com base no CEP
+	$('body').on('blur', '.address-form-new #ship-postal-code', function(evt) {
+		let cep = $(this).val();
+
 		$.ajax({
 			type: "GET",
-			url: "//api.postmon.com.br/v1/cep/" + _cep,
-			success: function(_address) {
-				var _bairro = _address.bairro,
-					_cidade = _address.cidade,
-					_endereco = _address.logradouro,
-					_estado = _address.estado;
-					
-				$(".address-form-new #ship-neighborhood").val(_bairro),
-				$(".address-form-new #ship-city").val(_cidade),
-				$(".address-form-new #ship-street").val(_endereco),
-				$(".address-form-new #ship-state-alternate").val(_estado)
+			url: "//api.postmon.com.br/v1/cep/" + cep,
+			success: function(address){
+				let _bairro = address.bairro,
+					_cidade = address.cidade,
+					_endereco = address.logradouro,
+					_estado = address.estado;
+				// Adiciona os dados adquiridos por meio da api
+				$("#ship-neighborhood").val(_bairro),
+				$("#ship-city").val(_cidade),
+				$("#ship-street").val(_endereco),
+				$("#ship-state-alternate").val(_estado);
+				// Encadeia um evento para ativar a verificação de preenchimento da vtex
+				$("#ship-neighborhood").trigger('keyup'),
+				$("#ship-city").trigger('keyup'),
+				$("#ship-street").trigger('keyup'),
+				$("#ship-state-alternate").trigger('keyup');
+				// Remove qualquer valor selecionado e aplica o novo
+				$('#ship-state').find('option').prop('selected', false);
+				$('#ship-state').find(`option[value="${_estado}"]`).prop('selected', true);
+				// Remove indicador de erro
+				$('#ship-postal-code').removeClass('error')
+			},
+			error: function(){
+				// Adiciona indicador de erro
+				$('#ship-postal-code').addClass('error');
 			}
 		})
 	});
-	$('#giftlistform input, #giftlistform select').on('keydown change', function(){
+	// Função para gerar automáticamente um valor para o endereço de compartilhamento da lista
+	$('#giftlistform input, #giftlistform select').on('keypress keyup change blur', function(){
 		let giftlisttype = $('#giftlisttype').find(':selected').text(),
 			giftlistname = $('#giftlistname').val(),
+			giftlisturl = $('#giftlisturl-site'),
 			creationdate = Date.now();
+
+		creationdate = String(creationdate).substr(0, 4);
 
 		let str = giftlisttype + giftlistname + creationdate;
 		// Remove acentuações e espaços
 		let url = str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-			url = str.replace(/\s/g, '');
+			url = str.replace(/\s/g, '').toLowerCase();
 
 		$('#giftlisturl').val(url);
-		$('#giftlisturl').trigger('change');
-	});
-	$("#giftlisttype").on('change', function(){
-		var _el = $(this);
-		console.log(_el.find(':selected').val())
-		if(_el.find(':selected').val() == 20){
-			$('.giftlisteventlocation, .giftlisteventcity').hide()
+
+		if(!!giftlisturl.attr('data-url')){
+			giftlisturl.text(giftlisturl.attr('data-url') + url);
 		}else{
-			$('.giftlisteventlocation, .giftlisteventcity').show()
+			giftlisturl.attr('data-url', giftlisturl.text());
+			giftlisturl.text(giftlisturl.attr('data-url') + url);
+		}
+	});
+	// Função para ocultar um ou mais campos com base na lista
+	$("#giftlisttype").on('change', function(){
+		// 20 - guideshop / 6 - lista de presentes / ...etc.
+		let skipCity = ['20', '6'],
+			selectedOption = $(this).find(':selected').val();
+
+		let giftlisteventcity = $('.giftlisteventlocation, .giftlisteventcity');
+
+		// Verifica se o valor se encontra na array acima
+		// -1 representa falso, qualquer outro número é a posição
+		if($.inArray(selectedOption, skipCity) >= 0){
+			giftlisteventcity.slideUp();
+		}else{
+			giftlisteventcity.slideDown();
 		}
 	});
 })
