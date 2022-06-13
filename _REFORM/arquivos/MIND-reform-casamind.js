0eccb0f8fe7c0369708ea0a20d92fa588bb1d6f6
williamcms <williamcms_@outlook.com>
2022-06-13 12:28:11 -0300

    Melhorias no código para funcionar como foi inicialmente planejado apesar das várias mudanças recentes e futuras

--- _REFORM/arquivos/MIND-reform-casamind.js
+++ _REFORM/arquivos/MIND-reform-casamind.js
@@ -2,7 +2,7 @@
  *	Código por William Di Biasi Bogik
  *
  */
-(isMobile = () => {
+var isMobile = (isMobile = () => {
 	let isMobile = false; //initiate as false
 	//device detection
 	if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
@@ -89,7 +89,7 @@ $(document).ready(function(){
 		}
 	})();
 	//Fecha o overlay/modal se clicar fora da área
-	$('.overlayform').on('click', function(e){
+	$(document).on('click', '.overlayform', function(e){
 		if($(e.target).is('.overlayform')){
 			$(this).fadeToggle('slow');
 		}
