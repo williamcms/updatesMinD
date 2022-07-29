d7f8c805e9545937ed83e8b0741c6948fa9e7b33
Williamcms <williamcms_@outlook.com>
2022-07-28 21:54:02 -0300

    Adição de constructor e modificadores de acessibilidade

--- VTEX IO/react-app-template/react/HelloWorld.tsx
+++ VTEX IO/react-app-template/react/HelloWorld.tsx
@@ -1,18 +1,23 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react'
 
-interface HelloProps{
-	name?: string,
-	font: string
+interface HelloProps {
+  name?: string
+  font: string
 }
 class HelloWorld extends Component<HelloProps> {
-	render(){
-		return (
-			<div className="tc">
-				<h2 className={this.props.font}>Olá, {this.props.name}!</h2>
-				<p className="i">Este é um ambiente de testes</p>
-			</div>
-		);
-	}
+  constructor(props: HelloProps | Readonly<HelloProps>) {
+    super(props)
+    this.state = {}
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div className="tc">
+        <h2 className={this.props.font}>Olá, {this.props.name}!</h2>
+        <p className="i">Este é um ambiente de testes!</p>
+      </div>
+    )
+  }
 }
 
 export default HelloWorld
