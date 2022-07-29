165ee8ed1e67832a5a2d8c6abacde96f47812350
Williamcms <williamcms_@outlook.com>
2022-07-28 21:01:59 -0300

    Alteração de HelloWorld para uma classe com props

--- VTEX IO/react-app-template/react/HelloWorld.tsx
+++ VTEX IO/react-app-template/react/HelloWorld.tsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react';
 
-const HelloWorld = () => <><h2>Hello, World!</h2></>
+interface HelloProps{
+	name?: string,
+	font: string
+}
+class HelloWorld extends Component<HelloProps> {
+	render(){
+		return (
+			<div className="tc">
+				<h2 className={this.props.font}>Olá, {this.props.name}!</h2>
+				<p className="i">Este é um ambiente de testes</p>
+			</div>
+		);
+	}
+}
 
 export default HelloWorld
